81e52d0ebbee0d90f6ba34c5e5346ba19a871e76
Rohith Nanda <nandarohith2001@gmail.com>
2023-10-25 09:56:42 +0530

    readded config filea

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "Rohith"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "ROHITH")
+        self.assertEqual(upper_name, "ROHIT")
 
 if __name__ == '__main__':
     unittest.main()
